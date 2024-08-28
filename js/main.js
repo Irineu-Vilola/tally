d84b1cb9121275b0c7ae06175aee36aa020d4580
Irineu-Vilola <irineumanuelvilda@gmail.com>
2024-08-28 14:26:02 +0100

    estragando

--- js/main.js
+++ js/main.js
@@ -10,12 +10,12 @@
    
    
    
-const menuDiv = document.getElementById('menu-mobile');
-const btnAnimar = document.getElementById('btn-menu');
+  const menuDiv = document.getElementById('menu-mobile');
+  const btnAnimar = document.getElementById('btn-menu');
 
     menuDiv.addEventListener('click', animarMenu)
 
-function animarMenu(){
+  function animarMenu(){
 
     menuDiv.classList.toggle('abrir')
     btnAnimar.classList.toggle('ativo')
