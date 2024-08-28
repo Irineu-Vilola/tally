d6948d29fe40eb88c077be0a07ef968c34f025ac
Irineu-Vilola <irineumanuelvilda@gmail.com>
2024-08-28 10:07:44 +0100

    mexendo

--- js/main.js
+++ js/main.js
@@ -20,3 +20,34 @@ function animarMenu(){
     menuDiv.classList.toggle('abrir')
     btnAnimar.classList.toggle('ativo')
     }
+
+
+
+     /**
+   * Testimonials slider
+   */
+  new Swiper('.testimonials-slider', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
+
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 20
+      }
+    }
+  });
